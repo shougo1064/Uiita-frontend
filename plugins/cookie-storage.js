144159d8d9ef9fb53a9a236a15f5870bb39ac3ec
依田尚悟 <>
2021-07-06 13:53:26 +0900

    cookie の値の取得方法を調整

--- plugins/cookie-storage.js
+++ plugins/cookie-storage.js
@@ -1,36 +1,26 @@
 import * as Cookies from 'js-cookie'
 import createPersistedState from 'vuex-persistedstate'
+import cookie from 'cookie'
+
 const COOKIE_TARGET_STORE = ['user']
 
-function filterValues(serializedJsonValue) {
-  const parsed = JSON.parse(serializedJsonValue)
-  const storeNames = Object.keys(parsed)
-
-  // 削除対象の store 一覧から cookie 保存する store を除外する
-  for (const storeName of COOKIE_TARGET_STORE) {
-    const idx = storeNames.indexOf(storeName)
-    storeNames.splice(idx, 1)
-  }
-
-  for (const key of storeNames) {
-    delete parsed[key]
-  }
-  return JSON.stringify(parsed)
-}
-
-export default ({ store, isDev }) => {
+export default ({ store, req, isDev }) => {
   createPersistedState({
     key: 'Uiita-frontend',
+    paths: COOKIE_TARGET_STORE,
     storage: {
-      getItem: (key) => Cookies.get(key),
-      setItem: (key, value) =>
-        Cookies.set(
-          key,
-          filterValues(value),
-          { expires: 30, secure: !isDev },
-          { samesite: 'lax' }
-        ),
-      removeItem: (key) => Cookies.remove(key),
+      getItem: (key) =>
+        process.client
+          ? Cookies.get(key)
+          : cookie.parse(req.headers.cookie)[key],
+      setItem: (key) => {
+        const cookie = Cookies.set(key, {
+          expires: 14,
+          secure: !isDev,
+          sameSite: 'lax',
+        })
+        return cookie
+      },
     },
   })(store)
 }
