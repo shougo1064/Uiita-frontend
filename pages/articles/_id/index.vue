930614d341fde0db254eb053ae007228f9dcbaa0
依田尚悟 <>
2021-07-06 13:33:13 +0900

    記事を書いたユーザーがログインユーザーだった場合に削除ボタンが表示されるように調整

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -10,9 +10,11 @@
             :auto-update="60"
           />
           <v-spacer></v-spacer>
-          <v-btn text fab small>
-            <v-icon color="#3085DE">fas fa-trash-alt</v-icon>
-          </v-btn>
+          <template v-if="isShowBtn">
+            <v-btn text fab small @click="deleteArticle">
+              <v-icon color="#3085DE">fas fa-trash-alt</v-icon>
+            </v-btn>
+          </template>
         </v-layout>
         <h1 :class="$style.article_title">{{ article.title }}</h1>
         <div :class="$style.article_body_container">
@@ -39,6 +41,14 @@ export default {
     },
   },
 
+  // 暫定でログインユーザーと記事を書いたユーザーが比較できる要素として設定
+  // 本来は user の id などをエンコードしておいて比較する方が良さそう
+  isShowBtn() {
+    const currentUserEmail = this.$store.getters['user/headers'].uid
+    const result = currentUserEmail === this.article?.user?.email
+
+    return result
+  },
   async created() {
     const articleId = this.$route.params.id
     await this.$store.dispatch('article/fetchArticle', articleId).then(() => {
