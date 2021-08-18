b82e9044cbb1ca0dd7d503d0b3d8eb0a5e4c86c0
依田尚悟 <>
2021-08-18 23:20:32 +0900

    Nuxt アプリを heroku へデプロイする

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://uiita.herokuapp.com',
   },
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
